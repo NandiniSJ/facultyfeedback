26dc9ae672338c22f069a761a29e9755183cc018
NandiniSJ <nandinisjadhav97@gmail.com>
2023-01-28 18:47:22 +0530

    initial commit

---
+++ src/main/java/com/example/facultyfeedback/FacultyfeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.facultyfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyfeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyfeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/facultyfeedback/FacultyfeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facultyfeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyfeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
